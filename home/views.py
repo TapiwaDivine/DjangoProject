292c4db789975add627809b7b0371fc570e00602
Ubuntu <ubuntu@ip-172-31-95-249.ec2.internal>
2019-08-10 22:35:50 +0000

    adding ajustments and dependencies

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from django.utils import timezone
 
 def home(request):
 #will render the home page
     return render(request, "index.html")
+    
+def services(request):
+#will render the home page
+    return HttpResponse("<h1>Hello World</h1>")
