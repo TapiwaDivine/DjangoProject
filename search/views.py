bcd654da4c89aede97b0421f72eac6a61d392b4b
TapiwaDivine <tdchipatiko07@gmail.com>
2019-12-09 09:41:20 +0000

    intial commit from new env

--- search/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from services.models import Feature
-
-# Create your views here.
-
-def do_search(request):
-    feature = Feature.objects.filter(name__icontains=request.GET['q'])
-    return render(request, 'display_allfeatures.html', {'features': feature})
